d86513df88dec220f32dc1bccc694794f330c310
이주연 <jy122032@gmail.com>
2025-06-02 10:02:30 +0900

    입출력과 사칙연산

---
+++ WEEK01/BOJ_2588.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    int b;
+    cin >> a >> b;
+
+    cout << a * (b % 10) << "\n"
+        << a * ((b/10) % 10) << "\n"
+        << a * (b/100) << "\n"
+        << a * b;
+
+    return 0;
+}
